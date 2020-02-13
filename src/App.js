81b75ea9713ebccf5aa4a4a3efade6b1b5709afe
mkleemoff <mkleemjr@gmail.com>
2020-02-12 19:37:31 -0500

    Changed the decrementByTwo() function after noticing it going to -1 by adding an && and an extra condition - tested and working.

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class Counter extends Component {
   }
 
   decrementByTwo = () => {
-    if (this.state.count > 0) {
+    if ((this.state.count > 0) && (this.state.count !== 1)) {
       this.setState({
         count: this.state.count - 2
       })
